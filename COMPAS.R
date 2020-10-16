1353fae1e1053ff0e081644c29c3cab619ee56e1
Sara Thiringer <s.thiringer@gmail.com>
2020-10-16 20:34:00 +0200

    Some plots from compas, although not the same as they get

--- COMPAS.R
+++ COMPAS.R
@@ -13,3 +13,23 @@ ggplot(compas, aes(x = Ethnic_Code_Text, y = RawScore)) +
 compas_2_years <- read.csv("data/compas-analysis-master/compas-scores-two-years.csv")
 
 summary(compas_2_years)
+
+
+library(grid)
+library(gridExtra)
+pblack <- ggplot(data=filter(compas_2_years, race =="African-American"), aes(ordered(decile_score))) + 
+  geom_bar() + xlab("Decile Score") +
+  ylim(0, 650) + ggtitle("Black Defendant's Decile Scores")
+pwhite <- ggplot(data=filter(compas_2_years, race =="Caucasian"), aes(ordered(decile_score))) + 
+  geom_bar() + xlab("Decile Score") +
+  ylim(0, 650) + ggtitle("White Defendant's Decile Scores")
+grid.arrange(pblack, pwhite,  ncol = 2)
+
+pblack_v <- ggplot(data=filter(compas_2_years, race =="African-American"), aes(ordered(v_decile_score))) + 
+  geom_bar() + xlab("Violent Decile Score") +
+  ylim(0, 700) + ggtitle("Black Defendant's Violent Decile Scores")
+pwhite_v <- ggplot(data=filter(compas_2_years, race =="Caucasian"), aes(ordered(v_decile_score))) + 
+  geom_bar() + xlab("Violent Decile Score") +
+  ylim(0, 700) + ggtitle("White Defendant's Violent Decile Scores")
+grid.arrange(pblack_v, pwhite_v,  ncol = 2)
+
