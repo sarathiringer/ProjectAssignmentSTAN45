436e8eb6acd3e19fca348d0afdfd42e2cd7e1507
Sara Thiringer <s.thiringer@gmail.com>
2020-10-22 23:02:49 +0200

    report, resampling, bib

--- R/biased_knn_lr.R
+++ R/biased_knn_lr.R
@@ -18,6 +18,8 @@ compas <-
   mutate(Two_yr_Recidivism = as.factor(ifelse(compas$Two_yr_Recidivism == '1', '0', '1'))) %>% 
   droplevels()
 
+y_numeric <- as.numeric(compas_train$Two_yr_Recidivism)-1
+
 summary(compas)
 
 split <- initial_split(compas, prop = 0.8, strata = "Two_yr_Recidivism")
@@ -113,7 +115,7 @@ ggplot(data = pred, aes(.pred_class, group = Ethnicity)) +
 # Create explainer by DALEX
 knn_explainer <- explain_tidymodels(knn_fitted, 
                                     data = compas_train[,-1], 
-                                    y = as.numeric(compas_train$Two_yr_Recidivism)-1,
+                                    y = y_numeric,
                                     label = "KNN")
 
 
@@ -200,7 +202,7 @@ ggplot(data = pred, aes(.pred_class, group = Ethnicity)) +
 # Create explainer by DALEX
 lr_explainer <- explain_tidymodels(lr_fitted, 
                                    data = compas_train[,-1], 
-                                   y = as.numeric(compas_train$Two_yr_Recidivism)-1,
+                                   y = y_numeric,
                                    label = "Logistic Regression")
 
 #########################
@@ -261,7 +263,7 @@ compas_train_mit <- compas_train %>%
 
 resamples <- vfold_cv(compas_train_mit, 5)
 
-## KNN mit
+## KNN mitigated
 
 knn_mod <- 
   nearest_neighbor(neighbors = tune()) %>% 
@@ -325,7 +327,7 @@ knn_mit_explainer <- explain_tidymodels(knn_mit_fitted,
                                         y = as.numeric(compas_train_mit$Two_yr_Recidivism)-1,
                                         label = "KNN Mit")
 
-## LR
+## LR mitigated
 
 lr_mod <- 
   logistic_reg(penalty = tune()) %>% 
@@ -408,10 +410,150 @@ plot(fobject_knn_lr_mit)
 ###### RESAMPLING ###########
 #############################
 
-uniform_indexes <- resample(protected = compas_train$Ethnicity,
+uniform_indexes <- fairmodels::resample(protected = compas_train$Ethnicity,
                             y = y_numeric)
 
+compas_resampled <- compas_train[uniform_indexes, ]
 
-resamples <- vfold_cv(compas_train[uniform_indexes, ], 5)
+resamples <- vfold_cv(compas_resampled, 5)
 
 
+## KNN resampled
+
+knn_mod <- 
+  nearest_neighbor(neighbors = tune()) %>% 
+  set_engine('kknn') %>% 
+  set_mode("classification")
+
+knn_rec <- 
+  recipe(Two_yr_Recidivism ~ ., data = compas_resampled) %>% 
+  step_bagimpute(everything()) %>%
+  step_scale(all_numeric(), -all_outcomes()) %>% 
+  step_dummy(Sex)
+
+knn_wf <- 
+  workflow() %>% 
+  add_model(knn_mod) %>% 
+  add_recipe(knn_rec)
+
+knn_res <- 
+  knn_wf %>% 
+  tune_grid(resamples = resamples,
+            metrics = metric_set(roc_auc, accuracy),
+            control = control_grid(save_pred = TRUE))
+
+knn_best <-
+  knn_res %>%
+  select_best(metric = "accuracy")
+
+knn_roc <- 
+  knn_res %>% 
+  collect_predictions(parameters = knn_best) %>% 
+  roc_curve(Two_yr_Recidivism, .pred_0) %>% 
+  mutate(model = "Ridge LR")
+
+knn_roc %>% 
+  ggplot(aes(x = 1 - specificity, y = sensitivity)) + 
+  geom_path() +
+  geom_abline(lty = 3) + 
+  coord_equal() 
+
+knn_acc <- 
+  knn_res %>% 
+  collect_predictions(parameters = knn_best) %>% 
+  accuracy(Two_yr_Recidivism, .pred_class) %>% 
+  mutate(model = "Ridge LR")
+
+knn_results <- 
+  knn_res %>% 
+  collect_predictions(parameters = knn_best)
+
+final_wf <-
+  finalize_workflow(knn_wf, knn_best)
+
+# Fit 
+knn_mit_resampled <- 
+  final_wf %>% 
+  fit(data = compas_resampled)
+
+# Explainer
+knn_res_explainer <- explain_tidymodels(knn_mit_resampled, 
+                                        data = compas_resampled[,-1], 
+                                        y = y_numeric,
+                                        label = "KNN Resampled")
+
+## LR resampled
+
+lr_mod <- 
+  logistic_reg(penalty = tune()) %>% 
+  set_engine("glmnet") %>% 
+  set_mode("classification")
+
+lr_rec <- 
+  recipe(Two_yr_Recidivism ~ ., data = compas_resampled) %>% 
+  step_rm(Ethnicity) %>% 
+  step_bagimpute(everything(), -all_outcomes()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_nzv(everything(), -all_outcomes()) %>%
+  step_normalize(everything(), -all_outcomes())
+
+lr_wf <- 
+  workflow() %>% 
+  add_model(lr_mod) %>% 
+  add_recipe(lr_rec)
+
+lr_res <- 
+  lr_wf %>% 
+  tune_grid(resamples = resamples,
+            metrics = metric_set(roc_auc, accuracy),
+            control = control_grid(save_pred = T))
+
+lr_best <-
+  lr_res %>%
+  select_best(metric = "accuracy")
+
+lr_roc <- 
+  lr_res %>% 
+  collect_predictions(parameters = lr_best) %>% 
+  roc_curve(Two_yr_Recidivism, .pred_0) %>% 
+  mutate(model = "Ridge LR")
+
+lr_roc %>% 
+  ggplot(aes(x = 1 - specificity, y = sensitivity)) + 
+  geom_path() +
+  geom_abline(lty = 3) + 
+  coord_equal() 
+
+lr_acc <- 
+  lr_res %>% 
+  collect_predictions(parameters = lr_best) %>% 
+  accuracy(Two_yr_Recidivism, .pred_class) %>% 
+  mutate(model = "Ridge LR")
+
+lr_results <- 
+  lr_res %>% 
+  collect_predictions(parameters = lr_best)
+
+final_wf <-
+  finalize_workflow(lr_wf, lr_best)
+
+# Fit 
+lr_mit_resampled <- 
+  final_wf %>% 
+  fit(data = compas_resampled)
+
+
+# Explainer
+lr_res_explainer <- explain_tidymodels(lr_mit_resampled, 
+                                       data = compas_resampled[,-1], 
+                                       y = as.numeric(compas_resampled$Two_yr_Recidivism)-1,
+                                       label = "Logistic Regression Resampled")
+
+
+## Fairness object
+fobject_knn_lr_res <- fairness_check(knn_explainer, lr_explainer, knn_res_explainer, lr_res_explainer,
+                                     protected = compas_resampled$Ethnicity,
+                                     privileged = 'Caucasian')
+
+print(fobject_knn_lr_res)
+plot(fobject_knn_lr_res)
