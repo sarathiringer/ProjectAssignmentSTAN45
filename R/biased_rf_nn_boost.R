e603b6a3c01d25d49cf4b9b2952d76de3ab8cd0c
Jonathan Rittmo <j.rittmo@gmail.com>
2020-10-26 16:00:18 +0100

    updated report

--- R/biased_rf_nn_boost.R
+++ R/biased_rf_nn_boost.R
@@ -11,7 +11,8 @@ rm(list = ls())
 set.seed(123)
 compas <- fairmodels::compas
 compas <- filter(compas, Ethnicity == "African_American" | Ethnicity == "Caucasian")
-compas$Two_yr_Recidivism <- as.factor(ifelse(compas$Two_yr_Recidivism == '1', '0', '1'))
+# compas$Two_yr_Recidivism <- as.factor(ifelse(compas$Two_yr_Recidivism == '1', '0', '1'))
+
 compas$Ethnicity <- droplevels(compas$Ethnicity)
 
 split <- initial_split(compas, prop = 0.8, strata = "Two_yr_Recidivism")
@@ -19,6 +20,7 @@ compas_train <- training(split)
 compas_test <- testing(split)
 
 y_numeric <- as.numeric(compas_train$Two_yr_Recidivism)-1
+y_numeric_test <- as.numeric(compas_test$Two_yr_Recidivism)-1
 
 resamples <- vfold_cv(compas_train, 5)
 
@@ -61,9 +63,12 @@ rf_fit <-
   final_wf %>%
   fit(data = compas_train)
 
+
 rf_explainer <- explain_tidymodels(rf_fit, data = compas_train[,-1],
                                    y = y_numeric,
                                    label = "RF")
+rf_test_exp <- update_data(rf_explainer, data = compas_test[,-1],
+                                   y = y_numeric_test, verbose = FALSE)
 
 # rf_compas <- ranger(Two_yr_Recidivism ~., data = compas, probability = TRUE)
 # rf_explainer <- DALEX::explain(rf_compas, data = compas[,-1], y = as.numeric(compas$Two_yr_Recidivism)-1, colorize = FALSE)
@@ -118,6 +123,8 @@ nn_fit <-
 nn_explainer <- explain_tidymodels(nn_fit, data = compas_train[,-1],
                                    y = y_numeric,
                                    label = "ANN")
+nn_test_exp <- update_data(nn_explainer, data = compas_test[,-1],
+                           y = y_numeric_test, verbose = FALSE)
 
 rm(nn_mod, nn_rec, nn_wf, nn_res, final_nn_wf, nn_best)
 
@@ -172,6 +179,9 @@ lr_explainer <- explain_tidymodels(lr_fit,
                                    data = compas_train[,-1],
                                    y = y_numeric, 
                                    label = "LR")
+lr_test_exp <- update_data(lr_explainer, data = compas_test[,-1],
+                           y = y_numeric_test, verbose = FALSE)
+
 rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 
 #######################
@@ -225,15 +235,47 @@ rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 #### BOOOSTING ########
 #######################
 
+ab_mod <- 
+  boost_tree(mtry = tune()) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("classification")
 
-df <- compas_train
-df$Two_yr_Recidivism <- as.numeric(compas_train$Two_yr_Recidivism)-1
-boost_fit <- gbm(Two_yr_Recidivism~., data = df) 
+ab_rec <- 
+  recipe(Two_yr_Recidivism ~ ., data = compas_train) %>% 
+  #step_bagimpute(everything(), all_outcomes()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_nzv(everything(), -all_outcomes()) %>%
+  step_normalize(everything(), -all_outcomes())
 
-boost_explainer <- DALEX::explain(boost_fit, data = compas_train[,-1],
+ab_wf <- 
+  workflow() %>% 
+  add_model(ab_mod) %>% 
+  add_recipe(ab_rec)
+
+ab_res <- 
+  ab_wf %>% 
+  tune_grid(resamples = resamples,
+            metrics = metric_set(roc_auc, accuracy),
+            control = control_grid(save_pred = T))
+
+ab_best <-
+  ab_res %>%
+  select_best(metric = "accuracy")
+
+final_ab_wf <- 
+  ab_wf  %>%
+  finalize_workflow(ab_best)
+
+ab_fit <- 
+  final_ab_wf %>%
+  fit(data = compas_train)
+
+boost_explainer <- DALEX::explain(ab_fit, data = compas_train[,-1],
                                   y = y_numeric,
                                   label = "AdaBoost")
-
+boost_test_exp <- update_data(boost_explainer, data = compas_test[,-1],
+                           y = y_numeric_test, verbose = FALSE)
+rm(ab_mod, ab_rec, ab_wf, ab_res, final_ab_wf, ab_best)
 
 #########################
 ####### METRICS #########
@@ -328,9 +370,11 @@ rf_fit <-
   final_wf %>%
   fit(data = compas_train_mit)
 
-rf_explainer_mit <- explain_tidymodels(rf_fit, data = compas_train_mit[,-1],
+rf_explainer_mit <- explain_tidymodels(rf_fit, data = compas_train[,-1],
                                    y = y_numeric,
                                    label = "RF rem")
+rf_test_exp_mit <- update_data(rf_explainer_mit, data = compas_test[,-1],
+                           y = y_numeric_test, verbose = FALSE)
 
 # rf_compas <- ranger(Two_yr_Recidivism ~., data = compas, probability = TRUE)
 # rf_explainer <- DALEX::explain(rf_compas, data = compas[,-1], y = as.numeric(compas$Two_yr_Recidivism)-1, colorize = FALSE)
@@ -386,9 +430,11 @@ nn_fit <-
   final_nn_wf %>%
   fit(data = compas_train_mit)
 
-nn_explainer_mit <- explain_tidymodels(nn_fit, data = compas_train_mit[,-1],
+nn_explainer_mit <- explain_tidymodels(nn_fit, data = compas_train[,-1],
                                    y = y_numeric,
                                    label = "ANN rem")
+nn_test_exp_mit <- update_data(nn_explainer_mit, data = compas_test[,-1],
+                               y = y_numeric_test, verbose = FALSE)
 
 rm(nn_mod, nn_rec, nn_roc, nn_wf, nn_res, final_nn_wf, nn_best)
 
@@ -435,9 +481,11 @@ lr_fit <-
 
 # Create explainer by DALEX
 lr_explainer_mit <- explain_tidymodels(lr_fit,
-                                   data = compas_train_mit[,-1],
+                                   data = compas_train[,-1],
                                    y = y_numeric, 
                                    label = "LR rem")
+lr_test_exp_mit <- update_data(lr_explainer_mit, data = compas_test[,-1],
+                               y = y_numeric_test, verbose = FALSE)
 rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 
 #######################
@@ -490,15 +538,47 @@ rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 #### BOOOSTING ########
 #######################
 
+ab_mod <- 
+  boost_tree(mtry = tune()) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("classification")
 
-df <- compas_train_mit
-df$Two_yr_Recidivism <- as.numeric(compas_train_mit$Two_yr_Recidivism)-1
-boost_fit <- gbm(Two_yr_Recidivism~., data = df) 
+ab_rec <- 
+  recipe(Two_yr_Recidivism ~ ., data = compas_train_mit) %>% 
+  #step_bagimpute(everything(), all_outcomes()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_nzv(everything(), -all_outcomes()) %>%
+  step_normalize(everything(), -all_outcomes())
 
-boost_explainer_mit <- DALEX::explain(boost_fit, data = compas_train_mit[,-1],
+ab_wf <- 
+  workflow() %>% 
+  add_model(ab_mod) %>% 
+  add_recipe(ab_rec)
+
+ab_res <- 
+  ab_wf %>% 
+  tune_grid(resamples = resamples,
+            metrics = metric_set(roc_auc, accuracy),
+            control = control_grid(save_pred = T))
+
+ab_best <-
+  ab_res %>%
+  select_best(metric = "accuracy")
+
+final_ab_wf <- 
+  ab_wf  %>%
+  finalize_workflow(ab_best)
+
+ab_fit <- 
+  final_ab_wf %>%
+  fit(data = compas_train_mit)
+
+boost_explainer_mit <- DALEX::explain(ab_fit, data = compas_train[,-1],
                                   y = y_numeric,
                                   label = "AdaBoost rem")
-
+boost_test_exp_mit <- update_data(boost_explainer_mit, data = compas_test[,-1],
+                              y = y_numeric_test, verbose = FALSE)
+rm(ab_mod, ab_rec, ab_wf, ab_res, final_ab_wf, ab_best)
 
 
 #########################
@@ -603,6 +683,8 @@ rf_fit <-
 rf_explainer_resa_uni <- explain_tidymodels(rf_fit, data = compas_train[,-1],
                                    y = y_numeric,
                                    label = "RF resa unif")
+rf_test_exp_resa_uni <- update_data(rf_explainer_resa_uni, data = compas_test[,-1],
+                               y = y_numeric_test, verbose = FALSE)
 
 # rf_compas <- ranger(Two_yr_Recidivism ~., data = compas, probability = TRUE)
 # rf_explainer <- DALEX::explain(rf_compas, data = compas[,-1], y = as.numeric(compas$Two_yr_Recidivism)-1, colorize = FALSE)
@@ -667,7 +749,8 @@ nn_fit <-
 nn_explainer_resa_uni <- explain_tidymodels(nn_fit, data = compas_train[,-1],
                                    y = y_numeric,
                                    label = "ANN resa unif")
-
+nn_test_exp_resa_uni <- update_data(nn_explainer_resa_uni, data = compas_test[,-1],
+                                    y = y_numeric_test, verbose = FALSE)
 rm(nn_mod, nn_rec, nn_roc, nn_wf, nn_res, final_nn_wf, nn_best)
 
 #######################
@@ -716,6 +799,8 @@ lr_explainer_resa_uni <- explain_tidymodels(lr_fit,
                                        data = compas_train[,-1],
                                        y = y_numeric, 
                                        label = "LR resa unif")
+lr_test_exp_resa_uni <- update_data(lr_explainer_resa_uni, data = compas_test[,-1],
+                                    y = y_numeric_test, verbose = FALSE)
 rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 
 #######################
@@ -768,16 +853,48 @@ rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 #### BOOOSTING ########
 #######################
 
+ab_mod <- 
+  boost_tree(mtry = tune()) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("classification")
 
-df <- compas_train[uniform_indexes, ]
-df$Two_yr_Recidivism <- as.numeric(compas_train[uniform_indexes, ]$Two_yr_Recidivism)-1
-boost_fit <- gbm(Two_yr_Recidivism~., data = df) 
+ab_rec <- 
+  recipe(Two_yr_Recidivism ~ ., data = compas_train[uniform_indexes, ]) %>% 
+  #step_bagimpute(everything(), all_outcomes()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_nzv(everything(), -all_outcomes()) %>%
+  step_normalize(everything(), -all_outcomes())
 
-boost_explainer_resa_uni <- DALEX::explain(boost_fit, data = compas_train[,-1],
+ab_wf <- 
+  workflow() %>% 
+  add_model(ab_mod) %>% 
+  add_recipe(ab_rec)
+
+ab_res <- 
+  ab_wf %>% 
+  tune_grid(resamples = resamples,
+            metrics = metric_set(roc_auc, accuracy),
+            control = control_grid(save_pred = T))
+
+ab_best <-
+  ab_res %>%
+  select_best(metric = "accuracy")
+
+final_ab_wf <- 
+  ab_wf  %>%
+  finalize_workflow(ab_best)
+
+ab_fit <- 
+  final_ab_wf %>%
+  fit(data = compas_train[uniform_indexes, ])
+
+boost_explainer_resa_uni <- DALEX::explain(ab_fit, data = compas_train[,-1],
                                   y = y_numeric,
                                   label = "AdaBoost resa unif")
+boost_test_exp_resa_uni <- update_data(boost_explainer_resa_uni, data = compas_test[,-1],
+                              y = y_numeric_test, verbose = FALSE)
 
-
+rm(ab_mod, ab_rec, ab_wf, ab_res, final_ab_wf, ab_best)
 
 #########################
 ####### METRICS #########
@@ -858,6 +975,8 @@ rf_fit <-
 rf_explainer_resa_pref <- explain_tidymodels(rf_fit, data = compas_train[,-1],
                                         y = y_numeric,
                                         label = "RF resa pref")
+rf_test_exp_resa_pref <- update_data(rf_explainer_resa_pref, data = compas_test[,-1],
+                                    y = y_numeric_test, verbose = FALSE)
 
 # rf_compas <- ranger(Two_yr_Recidivism ~., data = compas, probability = TRUE)
 # rf_explainer <- DALEX::explain(rf_compas, data = compas[,-1], y = as.numeric(compas$Two_yr_Recidivism)-1, colorize = FALSE)
@@ -923,6 +1042,9 @@ nn_explainer_resa_pref <- explain_tidymodels(nn_fit, data = compas_train[,-1],
                                         y = y_numeric,
                                         label = "ANN resa pref")
 
+nn_test_exp_resa_pref <- update_data(nn_explainer_resa_pref, data = compas_test[,-1],
+                                     y = y_numeric_test, verbose = FALSE)
+
 rm(nn_mod, nn_rec, nn_roc, nn_wf, nn_res, final_nn_wf, nn_best)
 
 #######################
@@ -971,6 +1093,8 @@ lr_explainer_resa_pref <- explain_tidymodels(lr_fit,
                                         data = compas_train[,-1],
                                         y = y_numeric, 
                                         label = "LR resa pref")
+lr_test_exp_resa_pref <- update_data(lr_explainer_resa_pref, data = compas_test[,-1],
+                                     y = y_numeric_test, verbose = FALSE)
 rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 
 #######################
@@ -1023,16 +1147,48 @@ rm(lr_mod, lr_rec, lr_wf, lr_res, final_lr_wf, lr_best)
 #### BOOOSTING ########
 #######################
 
+ab_mod <- 
+  boost_tree(mtry = tune()) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("classification")
 
-df <- compas_train[uniform_indexes, ]
-df$Two_yr_Recidivism <- as.numeric(compas_train[pref_ind, ]$Two_yr_Recidivism)-1
-boost_fit <- gbm(Two_yr_Recidivism~., data = df) 
+ab_rec <- 
+  recipe(Two_yr_Recidivism ~ ., data = compas_train[pref_ind, ]) %>% 
+  #step_bagimpute(everything(), all_outcomes()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_nzv(everything(), -all_outcomes()) %>%
+  step_normalize(everything(), -all_outcomes())
 
-boost_explainer_resa_pref <- DALEX::explain(boost_fit, data = compas_train[,-1],
-                                       y = y_numeric,
-                                       label = "AdaBoost resa pref")
+ab_wf <- 
+  workflow() %>% 
+  add_model(ab_mod) %>% 
+  add_recipe(ab_rec)
 
+ab_res <- 
+  ab_wf %>% 
+  tune_grid(resamples = resamples,
+            metrics = metric_set(roc_auc, accuracy),
+            control = control_grid(save_pred = T))
 
+ab_best <-
+  ab_res %>%
+  select_best(metric = "accuracy")
+
+final_ab_wf <- 
+  ab_wf  %>%
+  finalize_workflow(ab_best)
+
+ab_fit <- 
+  final_ab_wf %>%
+  fit(data = compas_train[pref_ind, ])
+
+boost_explainer_resa_pref <- DALEX::explain(ab_fit, data = compas_train[,-1],
+                                            y = y_numeric,
+                                            label = "AdaBoost resa pref")
+boost_test_exp_resa_pref <-  update_data(boost_explainer_resa_pref, data = compas_test[,-1],
+                                           y = y_numeric_test, verbose = FALSE)
+
+rm(ab_mod, ab_rec, ab_wf, ab_res, final_ab_wf, ab_best)
 
 #########################
 ####### METRICS #########
@@ -1046,61 +1202,75 @@ fobject_all <- fairness_check(fobject, rf_explainer_resa_pref, nn_explainer_resa
                           verbose = FALSE,
                           colorize = FALSE)
 
+fobject_test <- fairness_check(rf_test_exp, nn_test_exp, lr_test_exp, boost_test_exp,
+                               rf_test_exp_mit, nn_test_exp_mit, lr_test_exp_mit, boost_test_exp_mit,
+                               rf_test_exp_resa_uni, nn_test_exp_resa_uni, lr_test_exp_resa_uni, boost_test_exp_resa_uni,
+                               rf_test_exp_resa_pref, nn_test_exp_resa_pref, lr_test_exp_resa_pref, boost_test_exp_resa_pref ,
+                               protected = compas_test$Ethnicity,
+                               privileged = 'Caucasian',
+                               verbose = FALSE,
+                               colorize = FALSE)
+
+plot(performance_and_fairness(fobject_test, fairness_metric = "STP"))
 
 save(fobject1, file = "report/models/fobject1.Rdata")
 save(fobject_all, file = "report/models/fobject_all.Rdata")
+save(fobject_test, file = "report/models/fobject_test.Rdata")
 
 
 
-fobject_rfs <- fairness_check(rf_explainer_resa_pref,
+fobject_co <- fairness_check(rf_explainer_resa_uni, nn_explainer_resa_uni,
+                               boost_explainer_resa_uni, rf_explainer_resa_pref,
                                protected = compas_train$Ethnicity,
                                privileged = 'Caucasian',
                                verbose = FALSE,
                                colorize = FALSE)
-save(fobject_rfs, file = "report/models/fobject_rfs.Rdata")
+save(fobject_co, file = "report/models/fobject_co.Rdata")
 
-plot(ceteris_paribus_cutoff(fobject_rfs,
+co <- ceteris_paribus_cutoff(fobject_co,
+                            subgroup = "African_American",
+                            fairness_metrics = c("TPR", "STP"))
+plot(ceteris_paribus_cutoff(fobject_co,
                             subgroup = "African_American",
                             fairness_metrics = c("TPR","STP")))
 
 
 
-rf_comp <- fairness_check(rf_explainer_resa_pref, rf_explainer_resa_uni,
-                          rf_explainer,
+fo_comp <- fairness_check(nn_explainer_resa_uni, rf_explainer_resa_uni, rf_explainer_resa_pref,
+                          boost_explainer_resa_uni,
                           protected = compas_train$Ethnicity,
                           privileged = 'Caucasian',
                           verbose = FALSE,
                           colorize = FALSE)
-rf_comp <- fairness_check(rf_comp, rf_explainer_resa_pref, 
-                          label = "RF cutoff",
-                          protected = compas_train$Ethnicity,
-                          privileged = 'Caucasian',
-                          cutoff = list("African_American" = 0.42),
+fo_comp <- fairness_check(fo_comp, rf_explainer_resa_uni, 
+                          label = "RF resa unif cutoff",
+                          cutoff = list("African_American" = co[["min_data"]][["mins"]][1]),
                           verbose = FALSE,
                           colorize = FALSE)
+fo_comp <- fairness_check(fo_comp, nn_explainer_resa_uni, 
+                          label = "ANN resa unif cutoff",
+                          cutoff = list("African_American" = co[["min_data"]][["mins"]][2]),
+                          verbose = FALSE,
+                          colorize = FALSE)
+fo_comp <- fairness_check(fo_comp, boost_explainer_resa_uni, 
+                          label = "AdaBoost resa unif cutoff",
+                          cutoff = list("African_American" = co[["min_data"]][["mins"]][3]),
+                          verbose = FALSE,
+                          colorize = FALSE)
+fo_comp <- fairness_check(fo_comp, rf_explainer_resa_pref, 
+                          label = "RF resa pref cutoff",
+                          cutoff = list("African_American" = co[["min_data"]][["mins"]][4]),
+                          verbose = FALSE,
+                          colorize = FALSE)
+plot(performance_and_fairness(fo_comp, fairness_metric = "STP"))
 
-save(rf_comp, file = "report/models/rf_comp.Rdata")
-
-
-#plot(fobject)
-
-# cm <- choose_metric(fobject, c("TPR"))
-# plot(cm)
-
-
-# sm <- stack_metrics(fobject)
-# plot(sm)
-# 
-# 
-# fair_pca <- fairness_pca(fobject)
-# print(fair_pca)
-# plot(fair_pca)
-# 
-# fheatmap <- fairness_heatmap(fobject)
-# plot(fheatmap, text_size = 3)
+save(fo_comp, file = "report/models/fo_comp.Rdata")
 
 
-# model_performance(rf_explainer)
-# print(fobject, colorize = FALSE)
+#####################
+#### TESTING ########
+#####################
+
+?last_fit
 
 
