5e482c302c7ffe5aa13d88ccac3c50bcf2a8e206
Sara Thiringer <s.thiringer@gmail.com>
2020-10-26 22:50:38 +0100

    committing the right thing

--- R/plots.R
+++ R/plots.R
@@ -1,4 +1,62 @@
-load("/report/models/fobject1.Rdata")
+load("report/models/fobject1.Rdata")
 
 load("report/models/rf_comp.Rdata")
+
+fap <- performance_and_fairness(fobject1, fairness_metric = "STP",
+                                performance_metric = "accuracy")
+x <- plot(fap)
+x + 
+  ggtitle("") +
+  labs(x = "Accuracy", y = "Inversed parity loss (demographic parity)", color = "Model") +
+  geom_point(size = 5)
+
+
+
+
+df <- data.frame(fap$paf_data)
+
+ggplot(df, aes(x = performance_metric, y = fairness_metric, col = labels)) +
+  geom_point(size = 4) +
+  geom_text(aes(label=labels), hjust=1, vjust=2) +
+  labs(x = "Accuracy", y = "Inversed parity loss (demographic parity)", color = "Model")
+
+load("report/models/fobject_all.Rdata")
+fap_all <- performance_and_fairness(fobject_all, fairness_metric = "STP",
+                                performance_metric = "accuracy")
+
+x <- plot(fap_all)
+x + 
+  ggtitle("") +
+  labs(x = "Accuracy", y = "Inversed parity loss (demographic parity)") +
+  geom_point(aes(col = df$group), size = 3) +
+  scale_color_manual(name="Model",
+                      values=rep(brewer.pal(4, 'Set1'), 4),
+                      guide = guide_legend(override.aes=aes(fill=NA)))
+
+
+
+df <- data.frame(fap_all$paf_data)
+df$group <- rep(c("RF", "ANN", "AdaBoost", "LR"),4)
+df$method <- c(rep("Resampling pref", 4), 
+               rep("Resmapling uni", 4), 
+               rep("Disparate impact remover", 4), 
+               rep("None", 4))
+df$method_label <- c(rep("resa pref", 4), 
+                     rep("resa uni", 4), 
+                     rep("rem", 4), 
+                     rep("none", 4))
+
+df$jit<-with(df, ifelse(labels == "RF resa unif" | labels == "LR rem", 0.01, 0.02))
+
+ggplot(df, aes(x = performance_metric, y = fairness_metric, col = group)) +
+  geom_point(aes(shape = method), size = 4) +
+  geom_text(aes(label = method_label),
+                fontface = "bold",
+                position=position_jitter(width=df$jit,height=df$jit)) +
+  labs(x = "Accuracy", 
+       y = "Inversed parity loss (demographic parity)", 
+       color = "Model",
+       shape = "Bias Mitigation Method")
+
+
 
